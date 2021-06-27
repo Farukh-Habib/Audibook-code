3dc089ee9f8804c34b9e97c054b4380310a46943
Farukh-Habib <85098233+Farukh-Habib@users.noreply.github.com>
2021-06-27 21:18:44 +0530

    Add files via upload

---
+++ Audibooks.py
@@ -0,0 +1,43 @@
+
+                                                                                                                                                                                                                                                                                                                 
+# from _typeshed import OpenTextModeReading
+# import PyPDF2
+# from PyPDF2.generic import Bookmark
+# import pyttsx3 
+
+# book = open('D:\sample.pdf','rb')
+# pdfReader = PyPDF2.PdfFileReader(book)
+# pages = pdfReader.numPages
+# print(pages)
+
+# speker = pyttsx3.init()
+
+# page = pdfReader.getPage(6)
+# text = page.extractText()
+
+# speker.say(text)
+# speker.runAndWait() 
+
+
+from os import pardir
+import pyttsx3
+import PyPDF2
+from pyttsx3.engine import Engine
+
+
+# book = open('oop.pdf','rb')
+
+# pdfReader = PyPDF2.PdfFileReader(book)
+# pages = pdfReader.numPages
+# print(pages)
+
+
+# engine = pyttsx3.init()
+
+# page = pdfReader.getPage(7)
+# text = page.extractText()
+
+# engine.say(text)
+# engine.runAndWait()
+
+
